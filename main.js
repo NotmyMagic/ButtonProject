9865d0067eb6a78f6e15cc7638e91f626b565e38
NotmyMagic <56935337+NotmyMagic@users.noreply.github.com>
2024-09-20 13:01:20 -0600

    added css

---
+++ main.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".container");
+const rangeRed = document.querySelector("#rangeRed");
+const rangeGreen = document.querySelector("#rangeGreen");
+const rangeBlue = document.querySelector("#rangeBlue");
+const largeRedButton = document.querySelector(".large red button");
+let clicks = document.querySelector("#clicks");
+let clicks = 0;
+
+largeRedButton.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {}
