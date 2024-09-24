0f5d4cf5c80f176e0e0c490597a3a206c0140e59
NotmyMagic <56935337+NotmyMagic@users.noreply.github.com>
2024-09-24 14:00:29 -0600

    added JS for button click and background color

---
+++ main.js
@@ -0,0 +1,21 @@
+const container = document.querySelector(".container");
+const largeRedButton = document.querySelector(".large red button");
+let x = 1;
+
+function btnPressed(e) {
+  console.log("click");
+  let clicks = (document.querySelector("#clicks").innerHTML = `${x++}`);
+}
+
+function colorChange() {
+  let red = document.querySelector("#rangeRed").value;
+  let green = document.querySelector("#rangeGreen").value;
+  let blue = document.querySelector("#rangeBlue").value;
+  let RGB = `rbg(${red}, ${green}, ${blue})`;
+  document.body.style.backgroundColor = RGB;
+  console.log(`(${red}, ${green}, ${blue})`);
+}
+
+document.getElementById("rangeRed").addEventListener("input", colorChange);
+document.getElementById("rangeGreen").addEventListener("input", colorChange);
+document.getElementById("rangeBlue").addEventListener("input", colorChange);
